46602097897680b74bc4bff759888ecb413c7885
Nevascan <grossisamira@hotmail.com>
2023-02-16 21:00:41 -0300

    formatted code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,27 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "next/core-web-vitals", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:storybook/recommended",
+  ],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: "./tsconfig.json", //
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
   },
+  root: true,
   plugins: ["react", "@typescript-eslint"],
   rules: {
     "no-unused-expressions": "off",
-    indent: "error"
-  }
+    indent: "error",
+  },
 };
