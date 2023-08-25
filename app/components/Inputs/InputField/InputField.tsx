1418eebc2894b24b82f236b21e584b32d295db8b
Nevascan <grossisamira@hotmail.com>
2023-08-25 20:29:48 -0300

    fix: comments removed

--- app/components/Inputs/InputField/InputField.tsx
+++ app/components/Inputs/InputField/InputField.tsx
@@ -3,11 +3,6 @@ import { FC } from "react";
 import { StyledInputField } from "./InputField.styles";
 import { InputProps } from "props/inputs";
 
-// Refatorar o codigo --
-// Separar as props numa pasta --
-// Separar o input normal do formulario
-// Ver se consegue reaproveitar com ...props, ao inves de chamar todas
-
 export const InputField: FC<InputProps> = ({
   name = "",
   size = "large",
